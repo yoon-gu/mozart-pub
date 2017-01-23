d57cac82083ab1ef4ac9f0584c56aaa825aaafef
Yoonku Hwang <yz0624@gmail.com>
2017-01-23 18:35:16 +0900

    add testcase

---
+++ tests/test_sample.py
@@ -0,0 +1,15 @@
+import unittest
+import mozart as mz
+import numpy as np
+
+class TestStocMethods(unittest.TestCase):
+
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+
+	def test_isupper(self):
+		self.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+	def test_test(self):
+		self.assertTrue(True)
