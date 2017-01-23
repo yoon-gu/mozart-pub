5c0095e1605a61a761039ba741f6d416f692676f
Yoonku Hwang <yz0624@gmail.com>
2017-01-23 18:08:10 +0900

    add momzart module

---
+++ mozart/__init__.py
@@ -0,0 +1,33 @@
+"""
+Mozart
+===============
+
+``mozart`` is a software package written in ``Python`` for PDE Solver.
+
+Example
+-------
+Examples can be given using either the ``Example`` or ``Examples``
+sections. Sections support any reStructuredText formatting, including
+literal blocks.
+
+	$ python example_numpy.py
+
+
+Section breaks are created with two blank lines. Section breaks are also
+implicitly created anytime a new section starts. Section bodies *may* be
+indented.
+
+Notes
+-----
+	This is an example of an indented section. It's like any other section,
+	but the body is indented to help it stand out from surrounding text.
+
+If a section is indented, then a section break is created simply by
+resuming unindented text.
+
+"""
+__author__ = ("Yoon-gu Hwang <yz0624@gmail.com>", 
+	"Dong-Wook Shin <dwshin.yonsei@gmail.com>", 
+	"Ji-Yeon Suh <suh91919@naver.com>")
+
+# __all__ = ["models", "estimations", "epydemic_app"]
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Distribution, find_packages
+
+class BinaryDistribution(Distribution):
+	def is_pure(self):
+		return False
+
+setup(	name='mozart', version='0.0',
+		description='PDE Solver Package',
+		url='https://github.com/yoon-gu/mozart-pub/',
+		author='Yoon-gu Hwang',
+		author_email='yz0624@gmail.com',
+		license='MIT',
+		packages=find_packages(),
+		install_requires=[
+			'numpy',
+			'scipy',
+		],
+		include_package_data=True,
+		distclass=BinaryDistribution,
+		zip_safe=False)
