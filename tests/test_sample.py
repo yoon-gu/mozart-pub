e733307bf2fea1c26b4dccf3d3865a7486a9199c
Dong-wook Shin <nada1533@gmail.com>
2017-03-22 13:49:11 +0900

    1d mesh generator is added

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -14,6 +14,7 @@ class TestMethods(unittest.TestCase):
 		import mozart as mz
 		authors = ('Yoon-gu Hwang <yz0624@gmail.com>', 'Dong-Wook Shin <dwshin.yonsei@gmail.com>', 'Ji-Yeon Suh <suh91919@gmail.com>')
 		self.assertEqual(mz.__author__, authors)
+
 	def test_1d_uniform_mesh(self):
 		from mozart.mesh.rectangle import unit_interval
 		N = 4
@@ -22,6 +23,22 @@ class TestMethods(unittest.TestCase):
 		diff_n4e = n4e - np.array([[0,1], [1, 2], [2,3]])
 		self.assertTrue(LA.norm(diff_c4n) < 1E-8)
 		self.assertTrue(LA.norm(diff_n4e) < 1E-8)
+
+	def test_1d_uniform_interval(self):
+		from mozart.mesh.rectangle import interval
+		a=0
+		b=1
+		M=4
+		N=2
+		c4n, n4e, n4db, ind4e = interval(a,b,M,N)
+		diff_c4n = c4n - np.linspace(a,b,M*N+1)
+		diff_n4e = n4e - np.array([[0,2], [2,4], [4,6], [6,8]])
+		diff_n4db = n4db - np.array([0, 8])
+		diff_ind4e = ind4e - np.array([[0,1,2], [2,3,4], [4,5,6], [6,7,8]])
+		self.assertTrue(LA.norm(diff_c4n) < 1E-8)
+		self.assertTrue(LA.norm(diff_n4e) < 1E-8)
+		self.assertTrue(LA.norm(diff_n4db) < 1E-8)
+		self.assertTrue(LA.norm(diff_ind4e) < 1E-8)
 
 	def test_poisson_square_2d(self):
 		from mozart.mesh.rectangle import unit_square
