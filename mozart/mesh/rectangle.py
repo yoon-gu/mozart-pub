80119ae1d9687b3a4c8e6450b6c991526fb50338
Yoonku Hwang <yz0624@gmail.com>
2017-04-05 09:02:43 +0900

    remove sample.dat file and remove trailing spaces

--- mozart/mesh/rectangle.py
+++ mozart/mesh/rectangle.py
@@ -3,7 +3,7 @@ import numpy as np
 def interval(a, b, M, degree):
 	"""
 	Generates mesh information on an interval [a,b].
-	
+
 	Parameters
 		- ``a`` (``float``) : coordinate of left-end point of the interval
 		- ``b`` (``float``) : coordinate of right-end point of the interval
@@ -18,7 +18,7 @@ def interval(a, b, M, degree):
 
 	Example
 		>>> c4n, n4e, n4db, ind4e = interval(0,1,4,2)
-		>>> c4n 
+		>>> c4n
 		array([ 0.   ,  0.125,  0.25 ,  0.375,  0.5  ,  0.625,  0.75 ,  0.875,  1.   ])
 		>>> n4e
 		array([[0, 2],
@@ -41,29 +41,29 @@ def interval(a, b, M, degree):
 	return (c4n, n4e, n4db, ind4e)
 
 
-def rectangle(x1, x2, y1, y2, Mx, My, degree):  
-	"""  
-	Generates mesh information on the unit square [x1,x2]x[y1,y2]. 
+def rectangle(x1, x2, y1, y2, Mx, My, degree):
+	"""
+	Generates mesh information on the rectangle [x1,x2]x[y1,y2].
 
-	Parameters  
+	Parameters
 		- ``x1`` (``float``) : coordinate of left point on the x-axis
 		- ``x2`` (``float``) : coordinate of right point on the x-axis
 		- ``y1`` (``float``) : coordinate of bottom point on the y-axis
 		- ``y2`` (``float``) : coordinate of top point on the y-axis
-		- ``Mx`` (``int``) : the number of elements along x-axis  
-		- ``My`` (``int``) : the number of elements along y-axis  
-		- ``degree`` (``int``) : polynomial degree for the approximate solution 
+		- ``Mx`` (``int``) : the number of elements along x-axis
+		- ``My`` (``int``) : the number of elements along y-axis
+		- ``degree`` (``int``) : polynomial degree for the approximate solution
 
-	Returns  
-		- ``c4n`` (``float array``) : coordinates for nodes  
-		- ``n4e`` (``int array``) : nodes for elements  
-		- ``ind4e`` (``int array``) : indices for elements  
-		- ``n4Db`` (``int array``) : nodes for Dirichlet boundary 
+	Returns
+		- ``c4n`` (``float array``) : coordinates for nodes
+		- ``n4e`` (``int array``) : nodes for elements
+		- ``ind4e`` (``int array``) : indices for elements
+		- ``n4Db`` (``int array``) : nodes for Dirichlet boundary
 
-	Example  
-	""" 
+	Example
+	"""
 	import numpy as np
-	import numpy.matlib 
+	import numpy.matlib
 
 	Nx = degree*Mx + 1
 	Ny = degree*My + 1
@@ -77,19 +77,19 @@ def rectangle(x1, x2, y1, y2, Mx, My, degree):
 	c4n[1] = yy
 
 	ind4e = np.zeros(((degree+1)**2,Mx*My), dtype = np.int32)
-	tmp1 = np.matlib.repmat(np.arange(0,Mx*degree,degree),1,My) 
-	tmp2 =np.matlib.repmat(np.arange(0,(Mx*degree+1)*((My-1)*degree+1)-1,degree*(Mx*degree+1)),Mx,1)  
-	tmp1 = tmp1 + np.transpose(tmp2).flatten() 
-	for j in range(0,degree+1): 
-		ind4e[j*(degree+1)+np.arange(0,degree+1,1),:] = np.matlib.repmat(tmp1+j*(degree*Mx+1),degree+1,1) + np.transpose(np.matlib.repmat(np.arange(0,degree+1,1),Mx*My,1)) 
+	tmp1 = np.matlib.repmat(np.arange(0,Mx*degree,degree),1,My)
+	tmp2 =np.matlib.repmat(np.arange(0,(Mx*degree+1)*((My-1)*degree+1)-1,degree*(Mx*degree+1)),Mx,1)
+	tmp1 = tmp1 + np.transpose(tmp2).flatten()
+	for j in range(0,degree+1):
+		ind4e[j*(degree+1)+np.arange(0,degree+1,1),:] = np.matlib.repmat(tmp1+j*(degree*Mx+1),degree+1,1) + np.transpose(np.matlib.repmat(np.arange(0,degree+1,1),Mx*My,1))
 
-	n4e = ind4e[[0,degree,(degree+1)**2-1,degree*(degree+1)],:] 
+	n4e = ind4e[[0,degree,(degree+1)**2-1,degree*(degree+1)],:]
 
 
 	btm = np.arange(0,Nx,1)
 	top = np.arange(Nx*(Ny-1),Nx*Ny,1)
 	left = np.arange(0,Nx*(Ny-1)+1,Nx)
-	right = np.arange(Nx-1,Nx*Ny,Nx) 
+	right = np.arange(Nx-1,Nx*Ny,Nx)
 	n4Db = np.unique(np.concatenate((btm,top,left,right)))
 
 	return (c4n, ind4e, n4e, n4Db)
