5d118de3c9359dbff5abf272c4839594b5af897e
Dong-wook Shin <nada1533@gmail.com>
2017-04-10 13:21:38 +0900

    test code is fixed to cover n4NbNew

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -320,15 +320,15 @@ class TestFemTriangle(unittest.TestCase):
 		from mozart.poisson.fem.triangle import refineUniformRed
 		c4n = np.array([[0., 0.], [1., 0.], [1., 1.], [0., 1.], [0.5, 0.5]])
 		n4e = np.array([[0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 0, 4]])
-		n4Db = np.array([[0, 1], [1, 2], [2, 3], [3, 0]])
-		n4Nb = np.zeros((0,0), dtype = int)
+		n4Db = np.array([[0, 1], [1, 2]])
+		n4Nb = np.array([[2, 3], [3, 0]])
 		c4n, n4e, n4Db, n4Nb = refineUniformRed(c4n, n4e, n4Db, n4Nb)
 		diff_c4n = c4n - np.array([[0., 0.], [1., 0.], [1., 1.], [0., 1.], [0.5, 0.5], [0.5, 0.],
 			[1., 0.5], [0.5, 1.], [0., 0.5], [0.75, 0.25], [0.75, 0.75], [0.25, 0.75], [0.25, 0.25]])
 		diff_n4e = n4e - np.array([[0, 5, 12], [5, 1, 9], [9, 12, 5], [12, 9, 4], [1, 6, 9], [6, 2, 10], [10, 9, 6],
 			[9, 10, 4], [2, 7, 10], [7, 3, 11], [11, 10, 7], [10, 11, 4], [3, 8, 11], [8, 0, 12], [12, 11, 8], [11, 12, 4]])
-		diff_n4Db = n4Db - np.array([[0, 5], [5, 1], [1, 6], [6, 2], [2, 7], [7, 3], [3, 8], [8, 0]])
-		diff_n4Nb = n4Nb - np.zeros((0,2))
+		diff_n4Db = n4Db - np.array([[0, 5], [5, 1], [1, 6], [6, 2]])
+		diff_n4Nb = n4Nb - np.array([[2, 7], [7, 3], [3, 8], [8, 0]])
 		self.assertTrue(LA.norm(diff_c4n) < 1E-8)
 		self.assertTrue(LA.norm(diff_n4e) < 1E-8)
 		self.assertTrue(LA.norm(diff_n4Db) < 1E-8)
