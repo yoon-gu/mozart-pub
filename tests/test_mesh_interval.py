624fcfe7b997bcf157602d9afb9de9a00a469c7e
Yoonku Hwang <yz0624@gmail.com>
2017-04-16 11:27:27 +0900

    convinient to use numpy.testing for array equality testing : Resolving issue #42

--- tests/test_mesh_interval.py
+++ tests/test_mesh_interval.py
@@ -1,17 +1,14 @@
 import unittest
 import numpy as np
 from numpy import linalg as LA
+import numpy.testing as npt
 
-class TestFemInterval(unittest.TestCase):
+class TestFemMeshInterval(unittest.TestCase):
 	def test_1d_uniform_interval(self):
 		from mozart.mesh.interval import interval
 		a, b, M, N = (0, 1, 4, 2)
 		c4n, n4e, n4db, ind4e = interval(a,b,M,N)
-		diff_c4n = c4n - np.linspace(a,b,M*N+1)
-		diff_n4e = n4e - np.array([[0,2], [2,4], [4,6], [6,8]])
-		diff_n4db = n4db - np.array([0, 8])
-		diff_ind4e = ind4e - np.array([[0,1,2], [2,3,4], [4,5,6], [6,7,8]])
-		self.assertAlmostEqual(LA.norm(diff_c4n), 0.0, 8)
-		self.assertAlmostEqual(LA.norm(diff_n4e), 0.0, 8)
-		self.assertAlmostEqual(LA.norm(diff_n4db), 0.0, 8)
-		self.assertAlmostEqual(LA.norm(diff_ind4e), 0.0, 8)
+		npt.assert_almost_equal(c4n, np.linspace(a,b,M*N+1), decimal = 8)
+		npt.assert_almost_equal(n4e, np.array([[0,2], [2,4], [4,6], [6,8]]), decimal = 8)
+		npt.assert_almost_equal(n4db, np.array([0, 8]), decimal = 8)
+		npt.assert_almost_equal(ind4e, np.array([[0,1,2], [2,3,4], [4,5,6], [6,7,8]]), decimal = 8)
