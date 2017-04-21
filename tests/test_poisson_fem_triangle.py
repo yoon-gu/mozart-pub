b0ddd7b47d483904623fe7e38ac6c8d134608ff4
Dong-wook Shin <nada1533@gmail.com>
2017-04-21 17:16:52 +0900

    test compute_s4e in triangle.py

--- tests/test_poisson_fem_triangle.py
+++ tests/test_poisson_fem_triangle.py
@@ -29,3 +29,11 @@ def test_compute_n4s():
 	ref_n4s = np.array([[1, 3], [2, 4], [4, 6], [5, 7], [3, 0], [1, 4], [2, 5],
 		[6, 3], [4, 7], [5, 8], [0, 1], [4, 3], [1, 2], [5, 4], [7, 6], [8, 7]], dtype = int)
 	npt.assert_almost_equal(n4s, ref_n4s, decimal=8)
+
+def test_compute_s4e():
+	from mozart.poisson.fem.triangle import compute_s4e
+	n4e = np.array([[1, 3, 0], [3, 1, 4], [2, 4, 1], [4, 2, 5], [4, 6, 3], [6, 4, 7], [5, 7, 4], [7, 5, 8]])
+	s4e = compute_s4e(n4e)
+	ref_s4e = np.array([[0, 4, 10], [0, 5, 11], [1, 5, 12], [1, 6, 13],
+		[2, 7, 11], [2, 8, 14], [3, 8, 13], [3, 9, 15]], dtype = int)
+	npt.assert_almost_equal(s4e, ref_s4e, decimal=8)
